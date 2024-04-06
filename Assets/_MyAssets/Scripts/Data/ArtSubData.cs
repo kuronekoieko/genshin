30b0210cfa3387fa3d513d95f24526575d1a3808
ieko <tomonari.ieko@gmail.com>
2024-04-06 15:07:30 +0900

    聖遺物の桁修正

--- Assets/_MyAssets/Scripts/Data/ArtSubData.cs
+++ Assets/_MyAssets/Scripts/Data/ArtSubData.cs
@@ -12,15 +12,15 @@ public class ArtSubData : BaseData
     {
         name = artifactData.id;
         skip = artifactData.skip;
-        crit_rate = artifactData.crit_rate;
-        crit_dmg = artifactData.crit_dmg;
+        crit_rate = artifactData.crit_rate * 0.001f;
+        crit_dmg = artifactData.crit_dmg * 0.001f;
         hp = artifactData.hp;
-        hp_rate = artifactData.hp_rate;
+        hp_rate = artifactData.hp_rate * 0.001f;
         atk = artifactData.atk;
-        atk_rate = artifactData.atk_rate;
+        atk_rate = artifactData.atk_rate * 0.001f;
         def = artifactData.def;
-        def_rate = artifactData.def_rate;
-        energy_recharge = artifactData.energy_recharge;
+        def_rate = artifactData.def_rate * 0.001f;
+        energy_recharge = artifactData.energy_recharge * 0.001f;
         elemental_mastery = artifactData.elemental_mastery;
     }
 
