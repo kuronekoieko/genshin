10190e7e490f46d74e2af76ef036bdd563adf467
ieko <tomonari.ieko@gmail.com>
2024-04-07 11:19:58 +0900

    会心率が100を超えたときに、サブ会心ダメがスコアを超えるバグ

--- Assets/_MyAssets/Scripts/Character/DmgCalc/Crit.cs
+++ Assets/_MyAssets/Scripts/Character/DmgCalc/Crit.cs
@@ -5,47 +5,40 @@ using UnityEngine;
 
 public class Crit
 {
-    float critRate;
-    float critDmg;
-    float subCritRate;
-    float subCritDmg;
+    float CritRate;
+    float CritDmg;
+    float SubCritRate;
+    float SubCritDmg;
     public float ExpectedCritDmg { get; }
     // この３つ重い
-    public string RateDmg { get => $"{critRate:F2}:{critDmg:F2}"; }
-    public string CritProportion { get => $"{(critDmg / critRate):F2}"; }
-    public string SubCrit { get => $"{subCritRate:F2}:{subCritDmg:F2}"; }
+    public string RateDmg { get => $"{CritRate:F2}:{CritDmg:F2}"; }
+    public string CritProportion { get => $"{(CritDmg / CritRate):F2}"; }
+    public string SubCrit { get => $"{SubCritRate:F2}:{SubCritDmg:F2}"; }
 
     public Crit(float critRate, float critDmg, ArtSubData artSub)
     {
-
-
-
         if (artSub.Exist == false)
         {
-            (subCritRate, subCritDmg) = GetSubCrits(critRate, critDmg, artSub.Score);
-            critRate += subCritRate;
-            critDmg += subCritDmg;
+            (SubCritRate, SubCritDmg) = GetSubCrits(critRate, critDmg, artSub.Score);
+            critRate += SubCritRate;
+            critDmg += SubCritDmg;
         }
         else
         {
-            subCritRate = artSub.crit_rate;
-            subCritDmg = artSub.crit_dmg;
-            critRate = Mathf.Clamp01(critRate);
+            SubCritRate = artSub.crit_rate;
+            SubCritDmg = artSub.crit_dmg;
         }
+        CritRate = Mathf.Clamp01(critRate);
+        CritDmg = critDmg;
 
-
-        float expectedCritDmg = (1 + critDmg) * critRate + 1 - critRate;
-
-        this.critRate = critRate;
-        this.critDmg = critDmg;
-
-        ExpectedCritDmg = expectedCritDmg;
+        ExpectedCritDmg = (1 + critDmg) * critRate + 1 - critRate;
     }
 
     (float, float) GetSubCrits(float critRate, float critDmg, float score)
     {
         float subCritRate = (score - 2f * critRate + critDmg) / 4f;
-        subCritRate = Mathf.Clamp(subCritRate, -critRate, 1 - critRate);
+        // subCritRate = Mathf.Clamp(subCritRate, -critRate, 1 - critRate);
+        subCritRate = Mathf.Clamp(subCritRate, 0, 1);
         float subCritDmg = score - 2f * subCritRate;
         return (subCritRate, subCritDmg);
     }
