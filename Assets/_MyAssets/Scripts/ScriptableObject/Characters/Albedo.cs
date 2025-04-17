131aefe5e1cfb3ce8b1d6f83d7d70aabfcb82760
tomonari.ieko <tomonari.ieko@gmail.com>
2025-04-17 19:05:18 +0900

    ありベド計算

--- Assets/_MyAssets/Scripts/ScriptableObject/Characters/Albedo.cs
+++ Assets/_MyAssets/Scripts/ScriptableObject/Characters/Albedo.cs
@@ -14,22 +14,25 @@ namespace so
 
             var expectedDamage_skill = ExpectedDamage.Single(data, AttackType.Skill, skillPerArray[0], referenceStatus: ReferenceStatus.Def);
 
+            var ed = expectedDamage_skill;
+
             Dictionary<string, string> result = new()
             {
                 ["武器"] = data.weapon.DisplayName,
                 ["聖遺物セット"] = data.artSetData.name,
                 ["聖遺物メイン"] = data.artMainData.name,
                 ["バフキャラ"] = data.partyData.name,
-                ["スキル期待値"] = expectedDamage_skill.ToString(),
-                ["攻撃力"] = data.atk.ToString(),
+                ["スキル期待値"] = ed.Result.ToString(),
+                ["防御力"] = data.def.ToString(),
                 ["HP"] = data.hp.ToString(),
-                ["バフ"] = data.dmg_bonus.ToString(),
-                ["会心ダメ期待値"] = expectedDamage_skill.Crit.ExpectedCritDmg.ToString(),
+                ["バフ"] = ed.DmgBonus.ToString(),
+                ["加算"] = ed.DmgAdd.ToString(),
+                ["会心ダメ期待値"] = ed.Crit.ExpectedCritDmg.ToString(),
                 ["熟知"] = data.elemental_mastery.ToString(),
-                ["率ダメ"] = expectedDamage_skill.Crit.RateDmg,
-                ["会心ダメ比率"] = expectedDamage_skill.Crit.CritProportion,
+                ["率ダメ"] = ed.Crit.RateDmg,
+                ["会心ダメ比率"] = ed.Crit.CritProportion,
                 ["聖遺物組み合わせ"] = data.artSubData.name,
-                ["サブステ"] = expectedDamage_skill.Crit.SubCrit.ToString(),
+                ["サブステ"] = ed.Crit.SubCrit.ToString(),
                 ["サブHP%"] = data.artSubData.hp_rate.ToString(),
                 ["サブHP"] = data.artSubData.hp.ToString(),
                 ["スコア"] = data.artSubData.Score.ToString()
