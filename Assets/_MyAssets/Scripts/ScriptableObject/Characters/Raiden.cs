893ed71b92910233f1c863500ed11af896e99d4d
tomonari.ieko <tomonari.ieko@gmail.com>
2025-05-01 12:58:47 +0900

    コメント記載

--- Assets/_MyAssets/Scripts/ScriptableObject/Characters/Raiden.cs
+++ Assets/_MyAssets/Scripts/ScriptableObject/Characters/Raiden.cs
@@ -6,29 +6,38 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Raiden", menuName = "Scriptable Objects/Raiden")]
 public class Raiden : BaseCharacterSO
 {
+    // 夢想の一太刀
     readonly float _burstMultiplier_first = 7.21f;
+    // 夢想の一心
     readonly float[] _burstMultiplier_normalAtks = new[] { 0.798f, 0.784f, 0.960f, 0.551f, 0.553f, 1.319f, };
 
-    float skill_burstDmgBonusPerEnergyCost = 0.3f * 0.01f;
-    float burst_ganrikiStuckPerMembersEnergyCost = 0.2f;
-    float burst_multiplierAddPerGanriki_first = 0.07f;
-    float burst_multiplierAddPerGanriki_normalAtks = 0.0131f;
-    float ganrikiStuckMax = 60;
+    readonly float skill_burstDmgBonusPerEnergyCost = 0.3f * 0.01f;
+    readonly float burst_ganrikiStuckPerMembersEnergyCost = 0.2f;
+    readonly float burst_multiplierAddPerGanriki_first = 0.07f;
+    readonly float burst_multiplierAddPerGanriki_normalAtks = 0.0131f;
+    readonly float ganrikiStuckMax = 60;
 
     float talent_electroDmgBonusPerERPercentage = 0.4f * 0.01f;
 
     public override Dictionary<string, string> CalcDmg(Data data)
     {
+        // スキル：元素エネルギーを基準に爆発バフがアップする
         data.burst_bonus += skill_burstDmgBonusPerEnergyCost * status.energyCost;
+
+        // 天賦：元素チャージ効率が100%を超えている場合、超えた分1%につき、雷バフ+0.4%
         float energyRechargePercentageOver100 = Mathf.Clamp(data.energy_recharge - 1f, 0, 10000) * 100f;
         float talent_electroDmgBonus = talent_electroDmgBonusPerERPercentage * energyRechargePercentageOver100;
         data.electro_bonus += talent_electroDmgBonus;
 
+        // 爆発
+        // 願力：雷電以外が爆発を使うと、元素エネルギーを基準に、願力が蓄積される
         float ganrikiStuck = Mathf.Clamp(data.partyData.MembersEnergyCostSum * burst_ganrikiStuckPerMembersEnergyCost, 0, ganrikiStuckMax);
 
+        // 願力の層数を基準に、夢想の一太刀のダメージ倍率をアップする
         float burst_multiplierAdd_first = burst_multiplierAddPerGanriki_first * ganrikiStuck;
         float burstMultiplier_first = _burstMultiplier_first + burst_multiplierAdd_first;
 
+        // 願力の層数を基準に、夢想の一心のダメージ倍率をアップする
         float burst_multiplierAdd_normalAtks = burst_multiplierAddPerGanriki_normalAtks * ganrikiStuck;
         float[] burstMultiplier_normalAtks = new float[_burstMultiplier_normalAtks.Length];
         for (int i = 0; i < burstMultiplier_normalAtks.Length; i++)
@@ -39,8 +48,6 @@ public class Raiden : BaseCharacterSO
 
         var ed_burst_first = ExpectedDamage.Single(data, AttackType.Burst, burstMultiplier_first);
         var ed_burst_normal = ExpectedDamage.Sum(data, AttackType.Burst, burstMultiplier_normalAtks);
-
-
 
 
         Dictionary<string, string> result = new()
