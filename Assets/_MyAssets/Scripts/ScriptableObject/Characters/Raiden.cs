7ed51861cfcb4b24593ec8deff7011b124927e6d
tomonari.ieko <tomonari.ieko@gmail.com>
2025-05-01 12:14:10 +0900

    天賦の雷バフ修正

--- Assets/_MyAssets/Scripts/ScriptableObject/Characters/Raiden.cs
+++ Assets/_MyAssets/Scripts/ScriptableObject/Characters/Raiden.cs
@@ -15,14 +15,14 @@ public class Raiden : BaseCharacterSO
     float burst_multiplierAddPerGanriki_normalAtks = 0.0131f;
     float ganrikiStuckMax = 60;
 
-    float talent_electroDmgBonusPerER = 0.4f * 0.01f;
+    float talent_electroDmgBonusPerERPercentage = 0.4f * 0.01f;
 
     public override Dictionary<string, string> CalcDmg(Data data)
     {
         data.burst_bonus += skill_burstDmgBonusPerEnergyCost * status.energyCost;
-        float talent_electroDmgBonus = talent_electroDmgBonusPerER * (1f - data.energy_recharge);
+        float energyRechargePercentageOver100 = Mathf.Clamp(data.energy_recharge - 1f, 0, 10000) * 100f;
+        float talent_electroDmgBonus = talent_electroDmgBonusPerERPercentage * energyRechargePercentageOver100;
         data.electro_bonus += talent_electroDmgBonus;
-
 
         float ganrikiStuck = Mathf.Clamp(data.partyData.MembersEnergyCostSum * burst_ganrikiStuckPerMembersEnergyCost, 0, ganrikiStuckMax);
 
@@ -51,8 +51,10 @@ public class Raiden : BaseCharacterSO
             ["爆発一発目期待値"] = ed_burst_first.Result.ToString(true),
             ["爆発通常合計期待値"] = ed_burst_normal.Result.ToString(true),
             ["願力"] = ganrikiStuck.ToString(true),
-            ["爆発一発目倍率アップ"] = ed_burst_first.Result.ToString(),
+            ["爆発一発目倍率アップ"] = burst_multiplierAdd_first.ToString(),
             ["爆発通常倍率アップ"] = burst_multiplierAdd_normalAtks.ToString(),
+            ["元チャ"] = data.energy_recharge.ToString(),
+            ["100%以上の元チャ[%]"] = energyRechargePercentageOver100.ToString(),
             ["天賦の雷バフ"] = talent_electroDmgBonus.ToString(),
             ["攻撃力"] = data.atk.ToString(true),
             ["加算"] = ed_burst_first.DmgAdd.ToString(true),
